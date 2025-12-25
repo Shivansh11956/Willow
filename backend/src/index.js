03f8610782d6944e5c3f11d93c2017a0e6e8182e
Aditya Singh <adityasingh17092004@gmail.com>
2025-12-26 01:52:45 +0530

    Setup Railway deployment: backend serves frontend

--- backend/src/index.js
+++ backend/src/index.js
@@ -55,10 +55,10 @@ app.use("/api/friends", friendRoutes);
 app.use("/api/users", friendRoutes);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  app.use(express.static(path.join(__dirname, "../public")));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+    res.sendFile(path.join(__dirname, "../public", "index.html"));
   });
 }
 
